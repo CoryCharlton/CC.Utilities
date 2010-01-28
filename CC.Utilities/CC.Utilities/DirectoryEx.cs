87986a7260c5c9e4f94a9c7528acb2c82cb196d4
machx0r_cp <SND\machx0r_cp>
2010-01-28 23:07:57 +0000

    Added the DirectoryEx class which has a more efficient Move() implementation. Added a Compare() method to the ImageExtensions class.

---
+++ CC.Utilities/CC.Utilities/DirectoryEx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+
+namespace CC.Utilities
+{
+    /// <summary>
+    /// Improves on the performance (or lack thereof) of the <see cref="Directory"/> class.
+    /// </summary>
+    public static class DirectoryEx 
+    {
+        //NOTE: The documentation for System.IO.Directory indicates Directory.Move() can be used on files or folders. My implementation below only supports folders are the recursive folder moving is the performance hit on the original implementation.
+        
+        #region Public Methods
+        /// <summary>
+        /// Moves a directory and it's contents to a new location.
+        /// </summary>
+        /// <param name="sourceDirName">The path of the directory to move.</param>
+        /// <param name="destDirName">The path to the new location</param>
+        public static void Move(string sourceDirName, string destDirName)
+        {
+            Move(sourceDirName, destDirName, false, true);
+        }
+
+        /// <summary>
+        /// Moves a directory and it's contents to a new location.
+        /// </summary>
+        /// <param name="sourceDirName">The path of the directory to move.</param>
+        /// <param name="destDirName">The path to the new location</param>
+        /// <param name="overwrite">true if the destination can be overwritten; otherwise false</param>
+        public static void Move(string sourceDirName, string destDirName, bool overwrite)
+        {
+            Move(sourceDirName, destDirName, overwrite, true);
+        }
+
+        /// <summary>
+        /// Moves a directory and it's contents to a new location.
+        /// </summary>
+        /// <param name="sourceDirName">The path of the directory to move.</param>
+        /// <param name="destDirName">The path to the new location</param>
+        /// <param name="overwrite">true if the destination can be overwritten; otherwise false</param>
+        /// <param name="moveEmptyFolders"></param>
+        public static void Move(string sourceDirName, string destDirName, bool overwrite, bool moveEmptyFolders)
+        {
+#if DEBUG
+            DateTime enterTime = Logging.EnterMethod("CC.Utilities.DirectoryEx.Move()");
+#endif
+            if (sourceDirName == null || destDirName == null)
+            {
+                throw new ArgumentNullException((sourceDirName == null) ? "sourceDirName" : "destDirName", "sourceDirName or destDirName is a null reference");
+            }
+
+            bool containsInvalidPathChar = false;
+
+            foreach (char invalidPathChar in Path.GetInvalidPathChars())
+            {
+                if (destDirName.Contains(invalidPathChar.ToString()) || sourceDirName.Contains(invalidPathChar.ToString()))
+                {
+                    containsInvalidPathChar = true;
+                    break;
+                }
+            }
+
+            if (sourceDirName == string.Empty || destDirName == string.Empty || sourceDirName.ToLower() == destDirName.ToLower() || containsInvalidPathChar)
+            {
+                throw new ArgumentException("sourceDirName or destDirName is a zero-length string, contains only white space, or contains one or more invalid characters as defined by InvalidPathChars.");
+            }
+
+            if (sourceDirName.Length >= 248 || destDirName.Length >= 248)
+            {
+                throw new PathTooLongException("The specified path, file name, or both exceed the system-defined maximum length.");
+            }
+
+            if (!overwrite && Directory.Exists(destDirName))
+            {
+                throw new IOException("destDirName already exists: " + destDirName);
+            }
+
+            if (!Directory.Exists(sourceDirName))
+            {
+                throw new DirectoryNotFoundException("The path specified by sourceDirName is invalid (for example, it is on an unmapped drive): " + sourceDirName);
+            }
+
+            // All is good?
+
+            if (!sourceDirName.EndsWith("\\"))
+            {
+                sourceDirName += "\\";
+            }
+
+            if (!destDirName.EndsWith("\\"))
+            {
+                destDirName += "\\";
+            }
+
+            string[] subDirectories = Directory.GetDirectories(sourceDirName);
+            Array.Sort(subDirectories);
+
+            string[] files = Directory.GetFiles(sourceDirName);
+            Array.Sort(files);
+
+            //NOTE: The recursision here can be *HEAVY* on the stack depending on the number of folders. Going to look into using a Queue<T>. The only drawback from a Queue<T> would be if Directory.Delete(sourceDirName, true) is also slow like the default Move(). With a Queue<T> I can move all the files and folders individually but will only get one Delete() operation. Time for some testing...
+            if (moveEmptyFolders || (subDirectories.Length > 0 || files.Length > 0))
+            {
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
+
+                foreach (string subDirectory in subDirectories)
+                {
+                    Move(subDirectory, subDirectory.Replace(sourceDirName, destDirName), overwrite, moveEmptyFolders);
+                }
+
+                foreach (string file in files)
+                {
+                    File.Copy(file, file.Replace(sourceDirName, destDirName), overwrite);
+                }
+            }
+
+            Directory.Delete(sourceDirName, true);
+#if DEBUG
+            Logging.ExitMethod("CC.Utilities.DirectoryEx.Move()", enterTime);
+#endif
+        }
+        #endregion
+    }
+}
--- CC.Utilities/CC.Utilities/Extensions/ImageExtensions.cs
+++ CC.Utilities/CC.Utilities/Extensions/ImageExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
 namespace CC.Utilities
@@ -23,21 +25,21 @@ namespace CC.Utilities
             {
                 throw new ArgumentOutOfRangeException("brightness", brightness, "Brightness must be between -1.0 and 1.0");
             }
-            
+
             Bitmap outputBitmap = new Bitmap(image.Width, image.Height, image.PixelFormat);
 
             using (Graphics graphics = Graphics.FromImage(outputBitmap))
             {
                 using (ImageAttributes imageAttributes = new ImageAttributes())
                 {
-                    ColorMatrix colorMatrix = new ColorMatrix(new []
-                                                             {
-                                                                 new [] {1f, 0f, 0f, 0f, 0f},
-                                                                 new [] {0f, 1f, 0f, 0f, 0f},
-                                                                 new [] {0f, 0f, 1f, 0f, 0f},
-                                                                 new [] {0f, 0f, 0f, 1f, 0f},
-                                                                 new [] {brightness, brightness, brightness, 0f, 1f}
-                                                             });
+                    ColorMatrix colorMatrix = new ColorMatrix(new[]
+                                                                  {
+                                                                      new[] {1f, 0f, 0f, 0f, 0f},
+                                                                      new[] {0f, 1f, 0f, 0f, 0f},
+                                                                      new[] {0f, 0f, 1f, 0f, 0f},
+                                                                      new[] {0f, 0f, 0f, 1f, 0f},
+                                                                      new[] {brightness, brightness, brightness, 0f, 1f}
+                                                                  });
 
                     imageAttributes.SetColorMatrix(colorMatrix);
                     graphics.DrawImage(image, image.GetRectangle(), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
@@ -61,19 +63,19 @@ namespace CC.Utilities
             }
 
             Bitmap outputBitmap = new Bitmap(image.Width, image.Height, image.PixelFormat);
-            
+
             using (Graphics graphics = Graphics.FromImage(outputBitmap))
             {
                 using (ImageAttributes imageAttributes = new ImageAttributes())
                 {
                     ColorMatrix colorMatrix = new ColorMatrix(new[]
-                                                             {
-                                                                 new [] {contrast, 0f, 0f, 0f, 0f},
-                                                                 new [] {0f, 1f, contrast, 0f, 0f},
-                                                                 new [] {0f, 0f, 1f, contrast, 0f},
-                                                                 new [] {0f, 0f, 0f, 1f, 0f},
-                                                                 new [] {0f, 0f, 0f, 0f, 1f}
-                                                             });
+                                                                  {
+                                                                      new[] {contrast, 0f, 0f, 0f, 0f},
+                                                                      new[] {0f, 1f, contrast, 0f, 0f},
+                                                                      new[] {0f, 0f, 1f, contrast, 0f},
+                                                                      new[] {0f, 0f, 0f, 1f, 0f},
+                                                                      new[] {0f, 0f, 0f, 0f, 1f}
+                                                                  });
 
                     imageAttributes.SetColorMatrix(colorMatrix);
                     graphics.DrawImage(image, image.GetRectangle(), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
@@ -81,6 +83,96 @@ namespace CC.Utilities
             }
 
             return outputBitmap;
+        }
+
+        /// <summary>
+        /// Compare one image to another.
+        /// </summary>
+        /// <param name="image">The source image.</param>
+        /// <param name="other">The target image.</param>
+        /// <returns>0 if the images are the same, -1 if the image size differs, 1 if the pixels are different.</returns>
+        public static int Compare(this Image image, Image other)
+        {
+#if DEBUG
+            DateTime enterTime = Logging.EnterMethod("Image.Compare()");
+#endif
+            int returnValue = 0;
+
+            if (image == null && other == null)
+            {
+                returnValue = 0;
+            }
+            else if (image == null)
+            {
+                returnValue = -1;
+            }
+            else if (other == null)
+            {
+                returnValue = -1;
+            }
+            else if (image.Size != other.Size)
+            {
+                returnValue = -1;
+            }
+            else
+            {
+
+                //NOTE: This is slower than I'd like. Haven't debugged enough to determine where the slowdown is occuring and how to mitigate it.
+                ImageConverter imageConverter = new ImageConverter();
+                byte[] imageArray = (byte[]) imageConverter.ConvertTo(image, typeof (byte[]));
+                byte[] otherArray = (byte[]) imageConverter.ConvertTo(other, typeof (byte[]));
+
+                //NOTE: Testing if comparing the byte array is faster than hashing and then comparing the smaller hash
+
+                
+                SHA256Managed sha256Managed = new SHA256Managed();
+                byte[] imageHash = sha256Managed.ComputeHash(imageArray);
+                byte[] otherHash = sha256Managed.ComputeHash(otherArray);
+                
+
+                /*
+                MD5 md5 = MD5.Create();
+                byte[] imageHash = md5.ComputeHash(imageArray);
+                byte[] otherHash = md5.ComputeHash(otherArray);
+                */
+
+                /*
+                return (Encoding.UTF8.GetString(imageHash).CompareTo(Encoding.UTF8.GetString(otherHash)));
+                */
+
+                if (imageHash.Length != otherHash.Length)
+                {
+                    returnValue = 1;
+                }
+
+                for (int i = 0; i < imageHash.Length && i < otherHash.Length && returnValue == 0; i++)
+                {
+                    if (imageHash[i] != otherHash[i])
+                    {
+                        returnValue = 1;
+                    }
+                }
+
+                /*
+                if (imageArray.Length != otherArray.Length)
+                {
+                    returnValue = 1;
+                }
+
+                for (int i = 0; i < imageArray.Length && i < otherArray.Length && returnValue == 0; i++)
+                {
+                    if (imageArray[i] != otherArray[i])
+                    {
+                        returnValue = 1;
+                    }
+                }
+                */
+            }
+
+#if DEBUG
+            Logging.ExitMethod("Image.Compare()", enterTime);
+#endif
+            return returnValue;
         }
 
         /// <summary>
